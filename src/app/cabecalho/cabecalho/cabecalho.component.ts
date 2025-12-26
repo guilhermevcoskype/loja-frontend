7a5f3b8aec100fc1640eb919d29a2c91db3b20ea
Guilherme do Vale <guilhermevcoskype@gmail.com>
2025-12-26 09:50:10 -0300

    feat: inicio atualização para angular 20

--- src/app/cabecalho/cabecalho/cabecalho.component.ts
+++ src/app/cabecalho/cabecalho/cabecalho.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Component, computed, inject, OnDestroy, OnInit, signal } from '@angular/core';
 import { FormControl, ReactiveFormsModule } from '@angular/forms';
 import { Router, RouterLink } from '@angular/router';
 import {
@@ -11,6 +11,7 @@ import {
   Subscription,
   catchError,
   debounceTime,
+  distinctUntilChanged,
   filter,
   switchMap,
   throwError,
@@ -21,89 +22,54 @@ import { LoginService } from '../../conteudo/service/login.service';
 import { ProdutoService } from 'src/app/conteudo/service/produto.service';
 import { NgIf, NgClass, NgFor } from '@angular/common';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { toSignal } from '@angular/core/rxjs-interop';
+
 
 @Component({
     selector: 'app-cabecalho',
-    templateUrl: './cabecalho.component.html',
-    styleUrls: ['./cabecalho.component.css'],
     standalone: true,
-    imports: [
-        RouterLink,
-        ReactiveFormsModule,
-        FontAwesomeModule,
-        NgIf,
-        NgClass,
-        NgFor,
-    ],
+    imports: [FontAwesomeModule, ReactiveFormsModule, RouterLink],
+    templateUrl: './cabecalho.component.html',
+    styleUrls: ['./cabecalho.component.css']
 })
-export class CabecalhoComponent implements OnInit, OnDestroy {
+export class CabecalhoComponent implements OnInit {
+  // Injeção moderna (mais limpa)
+  private loginService = inject(LoginService);
+  private buscaService = inject(BuscaService);
+  private produtoService = inject(ProdutoService);
+  private router = inject(Router);
+
   faBusca = faSearch;
-  faBarras = faBars;
-  // faCoracao = faHeart;
+  faBars = faBars;
   faCarrinho = faShoppingCart;
-  logado: boolean = false;
-  nomeLogado!: string;
-  busca = new FormControl();
-  tokenDecodificado!: DadoToken;
-  mensagemErro = '';
-  public isCollapsed = false;
-  tipoProdutos: string[] = [];
-  loginSubscription!: Subscription;
-  buscaSubscription!: Subscription;
-  produtoSubscription!: Subscription;
+  // loginSubscription!: Subscription;
+  // buscaSubscription!: Subscription;
+  // produtoSubscription!: Subscription;
+  
 
-  constructor(
-    private router: Router,
-    private loginService: LoginService,
-    private buscaService: BuscaService,
-    private produtoService: ProdutoService
-  ) {}
+  // Signals para estado reativo
+  // toSignal converte o Observable do serviço em um Signal automaticamente
+  tokenDecodificado = toSignal(this.loginService.tokenDecodificado$, { initialValue: new DadoToken() });
+  
+  // Computed signal para verificar se está logado
+  logado = computed(() => this.tokenDecodificado().sub !== '');
 
-  fazerBusca$ = this.busca.valueChanges.pipe(
-    filter((valorDigitado) => valorDigitado.length >= 3),
-    debounceTime(300),
-    switchMap((valorDigitado) => {
-      this.buscaService.setBusca(valorDigitado, this.router);
-      return valorDigitado;
-    }),
-    catchError((erro) => {
-      console.log(erro);
-      return throwError(
-        () =>
-          new Error(
-            (this.mensagemErro =
-              'Ops, ocorreu um erro. Recarregue a aplicação!')
-          )
-      );
-    })
-  );
+  tipoProdutos = signal<string[]>([]);
+  isCollapsed = signal(false);
+  mensagemErro = signal('');
+
+  busca = new FormControl('', { nonNullable: true });
 
   ngOnInit(): void {
-    this.loginSubscription = this.loginService.tokenDecodificado$.subscribe((token) => {
-      this.tokenDecodificado = token;
-      if (token.sub != '') {
-        this.logado = true;
-      } else {
-        this.logado = false;
-      }
-    });
-    this.buscaSubscription = this.fazerBusca$.subscribe();
-    this.produtoSubscription = this.produtoService.buscarTiposProduto().subscribe({
-      next: (resultado) => {
-        this.tipoProdutos = resultado;
-      },
-      error: (erro) => console.log(erro),
-    });
-  }
-
-  ngOnDestroy(): void {
-    this.buscaSubscription.unsubscribe();
-    this.produtoSubscription.unsubscribe();
-    this.loginSubscription.unsubscribe();
-  }
-
-  public carregarTela(tela: string) {
-    this.router.navigate(['/conteudo/' + tela]);
+    // Carregar tipos de produto
+    this.produtoService.buscarTiposProduto().subscribe(res => this.tipoProdutos.set(res));
+    
+    // Lógica de busca (mantida como observable, pois busca reativa é melhor assim)
+    this.busca.valueChanges.pipe(
+      filter(v => v?.length >= 3),
+      debounceTime(300),
+      distinctUntilChanged() // Evita buscas repetidas se o texto não mudar
+    ).subscribe(valor => this.buscaService.setBusca(valor, this.router));
   }
 
   acaoBotaoSair() {
@@ -111,7 +77,17 @@ export class CabecalhoComponent implements OnInit, OnDestroy {
     this.loginService.setTokenDecodificado(new DadoToken());
   }
 
-  buscarTipoProduto(tipoProduto: string) {
+    buscarTipoProduto(tipoProduto: string) {
     this.buscaService.setBuscaPorTipo(tipoProduto, this.router);
+  }
+
+  // ngOnDestroy(): void {
+  //   this.buscaSubscription.unsubscribe();
+  //   this.produtoSubscription.unsubscribe();
+  //   this.loginSubscription.unsubscribe();
+  // }
+
+  public carregarTela(tela: string) {
+    this.router.navigate(['/conteudo/' + tela]);
   }
 }
